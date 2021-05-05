5c14c93a6084b98d868a14d6b4f8157d76dca016
ibuibu <ibuibu69@gmail.com>
2021-05-05 21:49:39 +0900

    Change header

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,6 +1,8 @@
 import React from "react"
 import { Link } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
+import Logo from "../assets/ojilogo.svg"
+import TopVideo from "../assets/topvideo.mp4"
 
 const Layout = ({ location, children }) => {
   const rootPath = `${__PATH_PREFIX__}/`
@@ -17,24 +19,17 @@ const Layout = ({ location, children }) => {
     header = (
       <div className="global-header-wrapper">
         <div className="navbar">
-          <div style={{ width: "150px", margin: "1.2rem", marginLeft: "5rem" }}>
-            <Link to="/">
-              <StaticImage
-                alt="logo"
-                src="../assets/logo.png"
-              ></StaticImage>
-            </Link>
-          </div>
-          <div className="navbar-link">
+          {/* <div className="navbar-link">
             <Link to="#">アバウト</Link>
             <Link to="#">SNS</Link>
-          </div>
+          </div> */}
         </div>
         <header className="global-header">
-          <Link to="/">
-            <h1>{pageName}</h1>
-          </Link>
+          <img src={Logo}/>
         </header>
+        <div className="top-video-wrapper">
+          <video className="top-video" src={TopVideo} playsInLine controls loop autoPlay muted/>
+        </div>
       </div>
     )
   } else {
