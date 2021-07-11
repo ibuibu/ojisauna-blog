a0267eb585a9a82ec93bf1eeb1fdc2779e178134
ibuibu <ibuibu69@gmail.com>
2021-07-11 18:06:02 +0900

    Adjust review-cards

--- src/components/review-cards.js
+++ src/components/review-cards.js
@@ -5,28 +5,36 @@ import { GatsbyImage, getImage } from "gatsby-plugin-image"
 const ReviewCard = props => {
   const reviews = props.reviews.nodes
   return (
-    <li className="post-list">
-      {reviews.map((review, idx) => {
-        const image = getImage(review.facilityImage)
-        const imageTitle = review.facilityImage.title
-        return (
-          <div key={idx}>
-            <Link to={"review/" + review.slug} itemProp="url">
-              <GatsbyImage
-                style={{
-                  width: "300px",
-                  display: "block",
-                  background: "black",
-                }}
-                image={image}
-                alt={imageTitle}
-              />
-              <p>{review.facilityName}</p>
-            </Link>
-          </div>
-        )
-      })}
-    </li>
+    <section>
+      <h1 style={{ textAlign: "center" }}>オジレビュー</h1>
+      <div className="review-cards">
+        {reviews.map((review, idx) => {
+          const image = getImage(review.facilityImage)
+          const imageTitle = review.facilityImage.title
+          return (
+            <div className="review-card" key={idx}>
+              <Link
+                to={"review/" + review.slug}
+                itemProp="url"
+                style={{ textDecoration: "none" }}
+              >
+                <GatsbyImage
+                  style={{
+                    width: "200px",
+                    display: "block",
+                    background: "black",
+                  }}
+                  imgStyle={{ borderRadius: "200px" }}
+                  image={image}
+                  alt={imageTitle}
+                />
+                <span className="review-card-title">No.{idx+1}{" "}{review.facilityName}</span>
+              </Link>
+            </div>
+          )
+        })}
+      </div>
+    </section>
   )
 }
 
